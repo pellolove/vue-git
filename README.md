7d6dbb29c3bc0a5115af1f8aa407d81bc825d60c
pellolove <pellolove@hotmail.com>
2017-04-03 11:09:04 +0800

    say hello to vue

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ $ vue init <template-name> <project-name>
 Example:
 
 $ vue init webpack my-project
+
+差点被坑哭了，原来想用eslint来进行检测，但是老是编译过去，就直接注释了代码。
+
+然后添加了一个vue-beautify的扩展
---
+++ photogallary/src/components/Hi.vue
@@ -0,0 +1,28 @@
+<template>
+    <div> 
+        <p>
+            你好 {{ msg }}
+            <h1> {{ time + '时间'}}</h1>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sayhello',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                time: new Date()
+    
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-algin: center;
+    }
+</style>
